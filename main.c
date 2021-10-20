9f06ad05173a6a817e5d6cfc0717ad0b544cbe36
luizlcezario <luizlcezario@gmail.com>
2021-10-19 23:53:00 -0300

    second_day

--- main.c
+++ main.c
@@ -20,7 +20,15 @@
 // 	*(unsigned int*)dst = color;
 // }
 
+/*
+	1.ler
+	2.verificar tamanho
+	3.verificar todo o map
 
+	map 
+
+
+*/
 int	key_hook(int keycode,t_vars *vars)
 {
 	if(keycode == KEY_ESC)
@@ -29,67 +37,181 @@ int	key_hook(int keycode,t_vars *vars)
 	return(0);
 }
 
-int	verify_map(char *name_map,t_map	*map)
+void	free_ptr(void *ptr)
+{
+	if (ptr != NULL)
+	{
+		free(ptr);
+		ptr = NULL;
+	}
+}
+
+int	ft_verify_str(char *str, char *ver, int max_search)
+{
+	int	a;
+
+	a = 0;
+	if (str == NULL || ver == NULL)
+		return (1);
+	while (*str != 0 && a <= max_search  && ft_strchr(ver, *str))
+		str++ && a++;
+	if(a == max_search)
+		return(0);
+	return(1);
+}
+// ok.verificar tamanho minimo 
+// letras erradas
+//  verify width
+// remocao do \n
+// verificar borda
+
+int verify_width_map(t_map *map, char *content, int heigth_now)
+{
+	if (map->width < 4)
+		return (1);
+	if (ft_verify_str(content, "1", map->width) == 0)
+	{
+		if (ft_strlen(content) != map->width && heigth_now > 0)
+			return (1);
+		if (ft_strlen(content) - 1 != map->width && heigth_now == 0)
+			return (1);
+		content[map->width] = '\0';
+		return(0);
+	}
+	else if (ft_verify_str(content, "10ECPJ", map->width) == 0)
+	{	
+		if (content [0] != '1' && content [map->width - 1] != '1')
+			return (1);
+		if (ft_strlen(content) - 1 != map->width)
+			return (1);
+		content[map->width] = '\0';
+		return(0);
+	}
+	return (1);
+}
+
+int	read_map(t_map *map)
+{
+	t_list	*tmp;
+	int		a;
+
+	map->map = malloc(1 * sizeof(t_list *));
+	tmp = malloc(1 * sizeof(t_list));
+	tmp->content = get_next_line(map->fd);
+	map->width = ft_strlen(tmp->content) - 1;
+	map->map[0] = tmp;
+	a = 0;
+	while(tmp->content != NULL)
+	{
+		if(verify_width_map(map, (char *) tmp->content, a) == 1)
+			return(1);
+		tmp->next = malloc(1 * sizeof(t_list));
+		tmp = tmp->next;
+		tmp->content = get_next_line(map->fd);
+		a++;
+	}
+	if(a < 4)
+		return (1);
+	map->height = a;
+	return(0);
+}
+
+
+int	verify_map(char *name_map, t_map *map)
 {
 	map->fd = open(name_map, O_RDONLY);
 	if (map->fd == -1)
 	{
 		perror("Error\n\033[1;31mMap doesn't exist\033[1;0m");
 		return (1);
+	} 
+	if(read_map(map) == 1)
+	{
+		perror("Error\n\033[1;31mMap size wrong\033[1;0m");
+		return (1);
 	}
 	return (0);
+}
+void destroy_map(t_map *map)
+{
+	ft_lstclear(map->map, &free_ptr);
+	free(map->map);
+}
+
+int	load_sprites(t_map *map, t_vars *vars, t_data_img **sprite)
+{
+	int	a;
+
+	a = 0;
+	while(a < 6)
+	{
+		ft_printf("<%s, %p>\n", g_sprite_path[a], vars->mlx);
+		sprite[a]->img = mlx_xpm_file_to_image(vars->mlx, g_sprite_path[a],
+				&sprite[a]->img_width, &sprite[a]->img_height);
+		// if(sprite[a]->img == NULL)
+		// 	return(1);
+		a++;
+	}
+	return(0);
+}
+
+int	start_game(t_map *map, t_vars *vars)
+{
+	t_data_img	**sprites;
+
+	sprites = malloc(7 * sizeof(t_data_img *));
+	sprites[6] = NULL;
+	if(sprites == NULL)
+		return(1);
+	vars->mlx = mlx_init();
+	if(vars->mlx == NULL)
+		return(1);
+	vars->win = mlx_new_window(vars->mlx, map->width * SPRITE_SIZE, 
+								map->height * SPRITE_SIZE, "so_long");
+	if(vars->win == NULL)
+		return(1);
+	if(load_sprites(map, vars, sprites) == 1)
+		return(1);
+	mlx_put_image_to_window(vars->mlx, vars->win, sprites[0]->img, 0 , 0);
+	mlx_loop(vars->mlx);
+	return(0);
 }
 
 int	main(int argc, char **argv)
 {
-	t_map	map;
-	t_vars vars;
-	t_data_img	test;
-	t_data_img	test2;
-	int error;
-		// void		*addr;
+	t_map		map;
+	t_vars		vars;
+
 	if(argc == 2)
 	{
-		if(verify_map(argv[1], &map) == 1)
-			return(1);
-		else
+		if (verify_map(argv[1], &map) == 1)
 		{
-			test.relative_path = "./sprites/facetattoo.xpm";
-			test2.relative_path = "./sprites/imagem.xpm";
-			vars.mlx = mlx_init();
-			vars.win = mlx_new_window(vars.mlx, 400, 400, "so_long");
-			test.img = mlx_xpm_file_to_image(vars.mlx, test.relative_path, &test.img_width, &test.img_height);
-			test2.img = mlx_xpm_file_to_image(vars.mlx, test2.relative_path, &test2.img_width, &test2.img_height);
-			mlx_put_image_to_window(vars.mlx, vars.win, test2.img, 0, 0);
-			mlx_put_image_to_window(vars.mlx, vars.win, test2.img, 100, 0);
-			mlx_put_image_to_window(vars.mlx, vars.win, test.img, 200, 0);
-			mlx_mouse_hook(vars.win, key_hook, &vars);
-			// if(error = -1)
-				// destroy_all(&vars, test, test2);
-			mlx_loop(vars.mlx);
+			destroy_map(&map);
+			return(-1);
+		}
+		if (start_game(&map, &vars) == 1)
+		{
+			perror("Error\n\033[1;31mErro no start game\033[1;0m");
+			return(-1);
+			// g_sprite_path = "./sprites/facetattoo.xpm";
+			// test2.relative_path = "./sprites/imagem.xpm";
+			// vars.mlx = mlx_init();
+			// vars.win = mlx_new_window(vars.mlx, 400, 400, "so_long");
+			// test.img = mlx_xpm_file_to_image(vars.mlx, test.relative_path, &test.img_width, &test.img_height);
+			// test2.img = mlx_xpm_file_to_image(vars.mlx, test2.relative_path, &test2.img_width, &test2.img_height);
+			// mlx_put_image_to_window(vars.mlx, vars.win, test2.img, 0, 0);
+			// mlx_put_image_to_window(vars.mlx, vars.win, test2.img, 100, 0);
+			// mlx_put_image_to_window(vars.mlx, vars.win, test.img, 200, 0);
+			// mlx_mouse_hook(vars.win, key_hook, &vars);
+			// // if(error = -1)
+			// 	// destroy_all(&vars, test, test2);
+			// mlx_loop(vars.mlx);
 		}
 	}
 	else
 	{
 		perror("Error\n\033[1;31mMap doesn't exist\033[1;0m");
-		return(1);
+		return(-1);
 	}
 	return (0);
 }
-
-
-// int main(void)
-// {
-// 	void *mlx;
-// 	void *mlx_win;
-// 	t_data	img;
-
-// 	mlx = mlx_init();
-// 	mlx_win = mlx_new_window(mlx, 100, 100, "Face Tattoo!");
-// 	img.img = mlx_new_image(mlx, 100, 100);
-// 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-// 								&img.endian);
-// 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-// 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 10);
-// 	mlx_loop(mlx);
-// }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: prafael- <prafaevanton@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 23:10:52 by prafael-          #+#    #+#             */
-/*   Updated: 2021/10/18 23:11:244 by prafae         ###   ########.fr       */
+/*   Updated: 2021/10/19 18:09:56 by prafae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,19 @@
 # include "minilibx/mlx.h"
 # include "libft/libft.h"
 
-# define KEY_ESC	65307
-# define KEY_A		97
-# define KEY_D		100
-# define KEY_S		115
-# define KEY_W		119
+# define KEY_ESC			65307
+# define KEY_A				97
+# define KEY_D				100
+# define KEY_S				115
+# define KEY_W				119
+# define SPRITE_SIZE		32
+
+char *g_sprite_path[6] = {"./assets/sprites/0.xpm", "./assets/sprites/1.xpm",
+"./assets/sprites/C.xpm", "./assets/sprites/E.xpm", "./assets/sprites/P.xpm",
+"./assets/sprites/J.xpm"};
 
 typedef struct s_map{
+	t_list	**map;
 	int		fd;
 	int		width;
 	int		height;
@@ -31,7 +37,6 @@ typedef struct s_map{
 
 typedef struct	s_data_img {
 	void	*img;
-	void	*relative_path;
 	int		img_width;
 	int		img_height;
 
